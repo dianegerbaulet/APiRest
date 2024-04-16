dd78423659f21b320363bca85cac2021387786ec
Diane Gerbaulet <diane.gerbaulet@esme.fr>
2024-04-16 23:40:13 +0200

    final

--- app/src/main/java/com/example/apirest/BMWI3.java
+++ app/src/main/java/com/example/apirest/BMWI3.java
@@ -1,20 +1,31 @@
 package com.example.apirest;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 
 public class BMWI3 extends AppCompatActivity {
     private CheckBox Favoris;
+    private Button buttonRetour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acheter_bmw_i3);
 
-        //Favoris = findViewById(R.id.Favoris);
+        Favoris = findViewById(R.id.Favoris);
+        buttonRetour = findViewById(R.id.button2);
+        buttonRetour.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(BMWI3.this, AcheterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
 
--- app/src/main/java/com/example/apirest/CitroenAmi.java
+++ app/src/main/java/com/example/apirest/CitroenAmi.java
@@ -1,12 +1,16 @@
 package com.example.apirest;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CitroenAmi extends AppCompatActivity {
     private CheckBox Favoris;
+    private Button buttonRetour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +18,12 @@ public class CitroenAmi extends AppCompatActivity {
         setContentView(R.layout.activity_acheter_citroen_ami);
 
         Favoris = findViewById(R.id.Favoris);
+        buttonRetour = findViewById(R.id.button);
+        buttonRetour.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(CitroenAmi.this, AcheterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
